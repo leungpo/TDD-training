5c475a7ec184142922d556a5180653c665f12555
leungpo <polly.leung@oocl.com>
2023-03-20 14:20:06 +0800

    init: first commit

---
+++ src/test/java/parking/ParkingLotTest.java
@@ -0,0 +1,14 @@
+package parking;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ParkingLotTest {
+
+	@Test
+	public void test_given_car_when_park_then_the_car_is_parked(){
+		Assert.assertEquals("Hello World", "Hello World");
+	}
+
+}
